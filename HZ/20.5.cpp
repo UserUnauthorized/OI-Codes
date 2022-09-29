4156fa862ccdb0ce4389f439234b36eac6318a7c
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2022-09-29 10:23:02 +0800

    20.5 & P2947

---
+++ HZ/20.5.cpp
@@ -0,0 +1,25 @@
+//HZ - 20.5
+//Luogu - P2947
+#include<bits/stdc++.h>
+using namespace std;
+int n,h[100005],up[100005];
+deque<int> que;
+void init();
+int main(){
+	init();
+	for(int i=n;i>=1;--i){
+		while(!que.empty() && h[que.back()] <= h[i]) que.pop_back();
+		if(!que.empty()) up[i] = que.back();
+		que.push_back(i);
+	}
+	for(int i=1;i<=n;++i)
+		printf("%d\n",up[i]);
+	return 0;
+}
+void init(){
+	memset(h,0,sizeof(h));
+	memset(up,0,sizeof(up));
+	scanf("%d",&n);
+	for(int i=1;i<=n;++i)
+		scanf("%d",h+i);
+}
