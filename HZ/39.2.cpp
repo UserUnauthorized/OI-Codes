fbe31110c741e7cf829ff0616b03be5a86a73910
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2022-10-20 09:37:17 +0800

    39.2

--- HZ/39.2.cpp
+++ HZ/39.2.cpp
@@ -7,7 +7,7 @@ constexpr int maxn = 1000006;
 struct Tree{
 	int l,r;
 	long long sum;
-	int lazy;
+	long long lazy;
 }tree[maxn<<2];
 struct EDGE{
 	int next;
@@ -18,7 +18,7 @@ void init();
 void build(int,int,int);
 void addEdge(int,int);
 //void update(int,int,int);
-void insert(int,int,int,int);
+void insert(int,int,int,long long);
 long long query(int,int,int);
 void dfs(int,int);
 int main(){
@@ -31,8 +31,9 @@ int main(){
 	for(int i = 0;i < m;++i){
 		scanf("%d",&order);
 		if(order == 1){
-			int pos,key;
-			scanf("%d%d",&pos,&key);
+			int pos;
+			long long key;
+			scanf("%d%lld",&pos,&key);
 			insert(1,dfn[pos],last[pos],key);
 		}else if(order == 2){
 			int pos;
@@ -52,6 +53,7 @@ void init(){
 	memset(seg,0,sizeof(seg));
 	
 	scanf("%d%d%d",&n,&m,&root);
+	
 	for(int i = 1;i <= n;++i){
 		scanf("%d",value + i);
 	}
@@ -74,31 +76,32 @@ void addEdge(int a,int b){
 	head[b] = cnt;
 }
 void dfs(int x,int fa){
-	dfn[x] = last[x] = ++cnt;
+	dfn[x] = ++cnt;
 	seg[cnt] = value[x];
 	
 	for(int i = head[x] ; i ; i = edge[i].next){
 		int to = edge[i].to;
 		if(to != fa){
 			dfs(to,x);
-			last[x] = max(last[x],last[to]);
 		}
 	}
+	
+	last[x] = cnt;
 }
 void build(int id,int l,int r){
 	tree[id].l=l;
 	tree[id].r=r;
 	
 	if(l==r){
-		tree[id].sum=seg[l];
+		tree[id].sum = seg[l];
 		return;
 	}
 	
 	int mid=(l+r)>>1;
-	build(lid,l,mid);
-	build(rid,mid+1,r);
+	build(id<<1,l,mid);
+	build(id<<1|1,mid+1,r);
 	
-	tree[id].sum=tree[lid].sum+tree[rid].sum;
+	tree[id].sum = tree[id<<1].sum + tree[id<<1|1].sum;
 }
 /*void update(int id,int pos,int key){
 	if(tree[id].l==tree[id].r){
@@ -130,7 +133,7 @@ long long query(int id,int l,int r){
 	else if(l>mid)return query(id<<1|1,l,r);
 	else return query(id<<1,l,r)+query(id<<1|1,l,r);
 }
-void insert(int id,int l,int r,int key){
+void insert(int id,int l,int r,long long key){
 	if(l<=tree[id].l && tree[id].r<=r){
 		tree[id].lazy += key;
 		tree[id].sum += key * (tree[id].r-tree[id].l+1);
