46a57b00cee41cdef4d5f36f6c33167d15cee3b3
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2022-10-22 09:58:32 +0800

    2022/10/22 10:00

---
+++ HZ-contests/263/B.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+//#include <D:\OI\OI-Codes\Template\debug.h>
+
+using namespace std;
+//using namespace DEBUG;
+
+int n;
+
+vector<int> v;
+
+void init();
+
+int main() {
+    init();
+    sort(v.begin(), v.end());
+    //debug(v);
+    while (v.size() > 1) {
+        int a = v.back();
+        v.pop_back();
+        int b = v.back();
+        v.pop_back();
+        v.push_back(max(a, b) - min(a, b));
+    }
+    printf("%d",v.at(0));
+    return 0;
+}
+
+void init() {
+    scanf("%d", &n);
+    v.resize(n);
+    for (auto iter = v.begin(); iter != v.end(); ++iter) {
+        scanf("%d", iter);
+    }
+}
