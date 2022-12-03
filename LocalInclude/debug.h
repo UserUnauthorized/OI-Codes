b342f542ee94d904e0d3620afbc0f2814ee0d85a
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2022-12-03 15:26:38 +0800

    Update debug.h

--- LocalInclude/debug.h
+++ LocalInclude/debug.h
@@ -1,22 +1,21 @@
-#include <bits/stdc++.h>
-
-using namespace std;
+#include <iostream>
+#include <vector>
 
 namespace DEBUG {
     template<typename T>
     inline void _debug(const char *format, T t) {
-        cerr << format << '=' << t << endl;
+        std::cerr << format << '=' << t << std::endl;
     }
 
     template<class First, class... Rest>
     inline void _debug(const char *format, First first, Rest... rest) {
-        while (*format != ',') cerr << *format++;
-        cerr << '=' << first << ",";
+        while (*format != ',') std::cerr << *format++;
+        std::cerr << '=' << first << ",";
         _debug(format + 1, rest...);
     }
 
     template<typename T>
-    ostream &operator<<(ostream &os, const vector<T> &V) {
+    std::ostream &operator<<(std::ostream &os, const std::vector<T> &V) {
         os << "[ ";
         for (const auto &vv: V) os << vv << ", ";
         os << "]";
