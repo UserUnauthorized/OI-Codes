7c424a77c05dfdb70a5cbd723dc44bbeebbe5994
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-01-27 21:34:06 +0800

    Update

--- HZ/31.3.cpp
+++ HZ/31.3.cpp
@@ -2,69 +2,71 @@
 //Luogu - P2704
 #include<bits/stdc++.h>
 using namespace std;
-constexpr int maxN = 102, maxS = 1 << 10, maxA = maxN << 2;
+constexpr int maxN = 102, maxS = 100;
 
-int n,m,cnt(0),zeroId(0);
-unsigned long long ans(0);
-bool dp[4][maxS][maxN * maxN];
-const bool emptyDp[maxS][maxN * maxN]{};
-unsigned int bit[maxS], sum[maxS], limit[maxS];
+struct STATUS{
+	int id;
+	unsigned int bit;
+	unsigned int sum;
+	
+	STATUS():id(0),bit(0),sum(0){};
+	STATUS(int _id_, unsigned int _bit_):id(_id_),bit(_bit_),sum(0){
+		while(_bit_ > 0){
+			if(_bit_ & 1)
+				++this->sum;
+		}
+	};
+	
+	bool operator&(const STATUS &Object) const{
+		return this->bit & Object.bit == 0;
+	}
+};
+
+vector<STATUS> status;
+
+int n,m,cnt(0);
+unsigned int ans(0);
+unsigned int dp[4][maxS][maxS];
+const bool emptyDp[maxS][maxS]{};
+unsigned int limit[maxN];
 
 inline void init();
-inline bool compatible(int x, int row);
-inline bool compatible(int x, int y, int z);
-inline bool check(unsigned int a,unsigned int b);
-void dfs(int x, int num, int current);
+inline bool check(unsigned int x, int row);
 
 int main(){
 	init();
-
-	for(int j = 1; j <= cnt; ++j) dp[1][j][sum[j]] = true;
-	dp[0][zeroId][0] = true;
 	
-	for(int i = 2; i <= n; ++i){
+	for(STATUS const &j : status)
+		if(check(j.bit, 1))
+			dp[1][j.id][0] = j.sum;
+			
+	for(STATUS const &j : status)
+		if(check(j.bit, 2))
+			for(STATUS const &x : status)
+				dp[2][x.id][j.id] = (j & x) ? j.sum + x.sum : 0;
+	
+	for(int i = 3; i <= n; ++i){ 
 		memset(dp[i % 3],0,sizeof(emptyDp));
-		for(int j = 1; j<= cnt; ++j){
-			if(!compatible(j, i)) continue;
-			/*for(int x = 1; x <= cnt; ++x){
-				for(int y = 1; y <= cnt; ++y){
-					if(compatible(j,x,y)){
-						for(int k = sum[j] + sum[x]; k <= maxA; ++k){
-							dp[i % 3][j][k] |= dp[(i - 1) % 3][x][k - sum[j]] && dp[(i - 2) % 3][y][k - sum[j] - sum[x]];
-						}
-					}
-				}
-			}*/
-			for(int x = 1; x <= cnt; ++x){
-				if(!check(bit[j], bit[x])) continue;
-				for(int k = sum[j] + sum[x]; k <= (i << 2); ++k){
-					if(!dp[(i - 1) % 3][x][k - sum[j]]) continue;
-					for(int y = 1; y <= cnt; ++y){
-						if(compatible(j,x,y) && dp[(i - 2) % 3][y][k - sum[j] - sum[x]]){
-							dp[i % 3][j][k] = true;
-							break;
-						}
-					}
-				}
-			}
-		}
+		
+		for(STATUS const &j : status)
+			if(check(j.bit, i))
+				for(STATUS const &x : status)
+					if(j & x)
+						for(STATUS const &y : status)
+							if(x & y && j & y)
+								dp[i % 3][j.id][x.id] = max(dp[i % 3][j.id][x.id], dp[(i - 1) % 3][x.id][y.id] + j.sum);
 	}
 	
-	for(int i = maxA; i >= 0; --i){
-		for(int j = 1; j <= cnt; ++j){
-			if(dp[n % 3][j][i]){
-				cout << i;
-				return 0;
-			}
-		}
-	}
-	return 0;			
+	for(STATUS const &j : status)
+		for(STATUS const &x : status)
+			ans = max(ans, dp[n % 3][j.id][x.id]);
+			
+	cout << ans;
+	return 0;	
 }
 
 void init(){
 	memset(dp,0,sizeof(dp));
-	memset(bit,0,sizeof(bit));
-	memset(sum,0,sizeof(sum));
 	memset(limit,0,sizeof(limit));
 
 	cin >> n >> m;
@@ -73,36 +75,18 @@ void init(){
 		for(int j = 0; j < m; ++j){
 			char ch;
 			cin >> ch;
-			if(ch == 'P')
+			if(ch == 'H')
 				limit[i] += 1 << j;
 		}
 	}
-
-	dfs(0,0,0);
-}
-
-void dfs(int x,int num,int current){
-	if(current >= m){
-		bit[++cnt] = x;
-		sum[cnt] = num;
-		if(num == 0)
-			zeroId = cnt;
-		return;
-	}
 	
-	dfs(x + (1 << current), num + 1, current + 3);
-	dfs(x, num, current + 1);
-	dfs(x, num, current + 2);
+	const int S = 1 << m;
+	for(int i = 0; i < S; ++i){
+		if((i & (i << 2))||(i & (i << 1))||(i & (i >> 1))||(i & (i >> 2))) continue;
+		status.emplace_back(i,cnt++);
+	}
 }
 
-bool compatible(int x, int row){
-	return check(bit[x], limit[row]);
-}
-
-bool compatible(int x, int y, int z){
-	return check(bit[z], bit[y]) && check(bit[z], bit[x]);
-}
-
-bool check(unsigned int a,unsigned int b){
-	return (a & (~b)) == a;
+bool check(unsigned int x, int row){
+	return (x & limit[row]) == 0;
 }
