f58b13e57742a19d26064b850b24e91e45360712
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-01-29 09:12:05 +0800

    31.4 & P3451

--- HZ/31.4.cpp
+++ HZ/31.4.cpp
@@ -1,35 +1,8 @@
 //HZ - 31.4
 //Luogu - P3451
 #include<bits/stdc++.h>
-#include<vector>
 using namespace std;
 constexpr int maxN = 2e4 + 5, maxM = 2e5 + 5, maxS = 1 << 19, maxK = 22;
-
-namespace DEBUG {
-    template<typename T>
-    inline void _debug(const char *format, T t) {
-        std::cerr << format << '=' << t << std::endl;
-    }
-
-    template<class First, class... Rest>
-    inline void _debug(const char *format, First first, Rest... rest) {
-        while (*format != ',') std::cerr << *format++;
-        std::cerr << '=' << first << ",";
-        _debug(format + 1, rest...);
-    }
-
-    template<typename T>
-    std::ostream &operator<<(std::ostream &os, const std::vector<T> &V) {
-        os << "[ ";
-        for (const auto &vv: V) os << vv << ", ";
-        os << "]";
-        return os;
-    }
-
-#define debug(...) _debug(#__VA_ARGS__, __VA_ARGS__)
-}  // namespace DEBUG
-
-using namespace DEBUG;
 
 struct EDGE{
 	int to;
