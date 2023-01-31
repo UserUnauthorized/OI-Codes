30f7a89d535897e3b06cbe09f2e0fb79ce2cdf3c
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-01-31 15:09:40 +0800

    P5025

--- Luogu/P5025.cpp
+++ Luogu/P5025.cpp
@@ -1,152 +1,117 @@
+//HZ - 28.10
 //Luogu - P5025
 #include<bits/stdc++.h>
-
 using namespace std;
-constexpr int maxn = 5e5 + 5, P = 1e9 + 7;
+constexpr int maxN = 5e5 + 5, MOD = 1e9 + 7;
+typedef long long posType;
+typedef unsigned int numType;
+array<posType,maxN << 3> leftBound,rightBound;
+array<numType,maxN> inId,outId;
+array<numType,maxN << 3> belong,size;
+array<bool,maxN << 3> vis;
 
-struct segTree {
-    int l, r;
+vector<int> edge[maxN << 3];
 
-    segTree() : l(-1), r(-1) {};
+struct NODE{
+	posType pos;
+	posType left;
+	posType right;
+	
+	NODE():pos(LONG_LONG_MAX),left(LONG_LONG_MIN),right(LONG_LONG_MAX){};
+	NODE(int _pos_, int _left_, int _right_):pos(_pos_),left(_left_),right(_right_){};
+} node[maxN];
 
-    segTree(int _l, int _r) : l(_l), r(_r) {};
-} tree[maxn << 2];
+class OUTREE{
+public:
+	void build(int n_, array<numType,maxN> &idArray);
+	void build_(int id, int l, int r, array<numType,maxN> &pos);
+private:
+	int _n_;
+} outTree;
 
-int n, sccnum(0), cnt(0), nodeCount(INT_MIN);
-long long treeId[maxn], dis[maxn << 2], pos[maxn], len[maxn];
-int dfn[maxn << 2], low[maxn << 2], belong[maxn << 2], size[maxn << 2], ans[maxn << 2];
-bool vis[maxn << 2], leaf[maxn << 2];
-stack<int> st;
-vector<int> edge[maxn << 2], sccEdge;
-vector<vector<int> > scc;
+class INTREE{
+public:
+	void build(int n_, array<numType,maxN> &idArray);
+	void build_(int id, int l, int r, array<numType,maxN> &pos);
+	void connect(int l, int r, int nodeId);
+	void connect_(int id, int l, int r, int queryL, int queryR, int nodeId);
+private:
+	int _n_;
+	static const int _base_ = maxN << 2;
+} inTree;
 
-void build(int id, int l, int r);
-
-void link(int id, int l, int r, const int &qL, const int &qR, const int &node);
-
-void tarjan(int x);
-
-void depthFirstSearch(int x);
+int N;
 
 void init();
+void tarjan(int x);
+void dfs(int x);
 
-int main() {
-    init();
-    build(1, 1, n);
-    for (int i = 1; i <= n; ++i) {
-        int _L(lower_bound(pos + 1, pos + n + 1, pos[i] - len[i]) - pos);
-        int _R(upper_bound(pos + 1, pos + n + 1, pos[i] + len[i]) - pos);
-        link(1, 1, n, _L, _R, treeId[i]);
-    }
-
-    for (int i = 1; i <= nodeCount; ++i)
-        if (!dfn[i])
-            tarjan(i);
-    //tarjan(1);
-
-    //return 114514;
-    cerr << "P\t" << sccnum << endl;
-    for (int i = 0; i < sccnum; ++i)
-        if (ans[i] == -1)
-            depthFirstSearch(i);
-
-    for (int i = 1; i <= n; ++i)
-        printf("%d\n", ans[belong[i]]);
-
-    return 0;
+int main(){
+	init();
 }
 
-void init() {
-    memset(vis, 0, sizeof(vis));
-    memset(leaf, 0, sizeof(leaf));
-    memset(ans, 0xff, sizeof(ans));
-
-    cin >> n;
-    for (int i = 1; i <= n; ++i)
-        cin >> pos[i] >> len[i];
-    pos[n + 1] = LONG_LONG_MAX;
+void init(){
+	cin >> n;
+	
+	outTree.build(n,outId);
+	inTree.build(n,inId);
 }
 
-void build(int id, int l, int r) {
-    cerr << "TREE\t" << id << "\t" << l << "\t" << r << endl;
-    if (l == r) {
-        treeId[r] = id;
-        leaf[id] = true;
-        nodeCount = max(nodeCount, id);
-        return;
-    }
-
-
-    int mid((l + r) >> 1);
-
-    build(id << 1, l, mid);
-    build(id << 1 | 1, mid + 1, r);
-
-    //if(l != mid)
-    edge[id].emplace_back(id << 1);
-    //if(mid + 1 != r)
-    edge[id].emplace_back(id << 1 | 1);
+void OUTREE::build(int n_, array<numType,maxN> &idArray){
+	this->_n_ = n_;
+	this->build_(1,1,_n_,idArray);
 }
 
-void link(int id, int l, int r, const int &qL, const int &qR, const int &node) {
-    if (qL <= l && r <= qR) {
-        edge[node].emplace_back(id);
-        return;
-    }
-
-    int mid((l + r) >> 1);
-
-    if (qL <= mid)
-        link(id << 1, l, mid, qL, qR, node);
-    if (qR > mid)
-        link(id << 1 | 1, mid + 1, r, qL, qR, node);
+void OUTREE::build_(int id, int l, int r, array<numType,maxN> &pos){
+	if(l == r){
+		pos[r] = id;
+		return;
+	}
+	
+	int mid((l + r) >> 1);
+	
+	this->build_(id << 1, l, mid, pos);
+	this->build_(id << 1|1, mid + 1, r, pos);
+	
+	edge[id << 1].emplace_back(id);
+	edge[id << 1|1].emplace_back(id);
 }
 
-void tarjan(int x) {
-    dfn[x] = low[x] = ++cnt;
-    vis[x] = true;
-    st.emplace(x);
-
-    for (const int &iter: edge[x]) {
-        if (!dfn[iter]) {
-            tarjan(iter);
-            low[x] = min(low[x], low[iter]);
-        } else if (vis[iter]) {
-            low[x] = min(low[x], dfn[iter]);
-        }
-    }
-
-    if (dfn[x] == low[x]) {
-        ++sccnum;
-        scc.emplace_back();
-        int y;
-        do {
-            y = st.top();
-            st.pop();
-            belong[y] = sccnum;
-            scc.back().emplace_back(y);
-            if (leaf[y])
-                ++size[sccnum];
-            //cout << "A\t" << y << "\t" << (leaf[y] ? 'Y' : 'N') << endl;
-        } while (y != x);
-    }
+void INTREE::build(int n_,ARRAY &idArray){
+	_n_ = n_;
+	this->build_(1,1,_n_,idArray);
 }
 
-void depthFirstSearch(int x) {
-    if (ans[x] != -1)
-        return;
+void INTREE::build_(int id, int l, int r, ARRAY &pos){
+	if(l == r){
+		pos[r] = id + _base_;
+		edge[id + _base_].emplace_back(id,0);
+		return;
+	}
+	
+	int mid((l + r) >> 1);
+	
+	this->build_(id << 1, l, mid, pos);
+	this->build_(id << 1|1, mid + 1, r, pos);
+	
+	edge[id + _base_].emplace_back((id << 1) + _base_, 0);
+	edge[id + _base_].emplace_back((id << 1|1) + _base_, 0);
+}
 
-    ans[x] = size[x];
+void INTREE::connect(int l, int r, int nodeId){
+	this->connect_(1,1,_n_,l,r,nodeId);
+}
 
-    cerr << "B\t" << x << "\t" << scc[x].size() << endl;
-    for (const int &k: scc[x]) {
-        cerr << "A\t" << k << endl;
-        for (const int &iter: edge[k]) {
-            cerr << "DEBUG\t" << iter << endl;
-            if (belong[iter]) {
-                depthFirstSearch(belong[iter] - 1);
-                ans[x] += ans[belong[iter] - 1];
-            }
-        }
-    }
+void INTREE::connect_(int id, int l, int r, int queryL, int queryR, int nodeId){
+	if(queryL <= l && r <= queryR){
+		edge[nodeId].emplace_back(id + _base_, 0);
+		return;
+	}
+	
+	int mid((l + r) >> 1);
+	
+	if(queryL <= mid)
+		this->connect_(id << 1, l, mid, queryL, queryR, nodeId);
+	if(queryR > mid)
+		this->connect_(id << 1|1, mid + 1, r, queryL, queryR, nodeId);
 }
