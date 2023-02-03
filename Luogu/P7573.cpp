2b5109600f26049dadf37b0e48899fa34bb62e9d
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-02-03 14:28:42 +0800

    P7573 「PMOI-3」公平の意

---
+++ Luogu/P7573.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int T;
+	cin >> T;
+	while(T--){
+		int n;
+		cin >> n;
+		if(n == 1) --n;
+		cout << (n + 1) / 2 << endl;
+	}
+	return 0;
+}
