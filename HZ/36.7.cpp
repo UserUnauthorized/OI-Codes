ea680deb71f349dfefeef4c605539f1d4dd153ba
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-02-10 10:39:08 +0800

    36.7 颜色 [Half]

--- HZ/36.7.cpp
+++ HZ/36.7.cpp
@@ -1,7 +1,7 @@
 //HZ - 36.7
 #include<bits/stdc++.h>
 using namespace std;
-constexpr int maxN = 5e4 + 5, maxM = 2e4 + 5, maxK = pow(maxN, 1.0 / 3.0) + 5;
+constexpr int maxN = 5e4 + 5, maxM = 2e4 + 5, maxK = 52;
 typedef int valueType;
 
 namespace DEBUG {
@@ -54,8 +54,6 @@ Query decode(Query Object, int lastAns);
 valueType query(Query x);
 
 int main(){
-	freopen("color2.in","r",stdin);
-	freopen("color2.ans","w",stdout);
 	init();
 	int lastAns(0);
 	while(Q--){
@@ -69,7 +67,7 @@ int main(){
 void init(){
 	cin >> N_ >> M_ >> Q;
 	
-	block_ = pow(N, 2.0 / 3.0);
+	block_ = 1000;
 	K_ = ceil((double)N / (double)block);
 	
 	for(int i = 0; i < N; ++i){
@@ -90,34 +88,15 @@ void init(){
 			++sum[k][source[i]];
 	}
 	
-//	for(int color = 1; color <= M; ++color)
-//		preAns[0][0][color] = preAns[0][0][color - 1] + sum[0][color] * sum[0][color];
-//
-//	for(int k = 0; k < K; ++k)
-//		for(int j = (k > 0) ? k : 1; j < K; ++j){
-//		for(int j = k; j < K; ++j){
-//			for(int color = 1; color <= M; ++color){
-//				preAns[k][j][color] = preAns[k][j][color - 1] + (sum[j][color] - sum[k - 1][color]) * (sum[j][color] - sum[k - 1][color]);
-//				debug(k,j,color,preAns[k][j][color],preAns[k][j][color - 1],sum[j][color],sum[k - 1][color]);
-//			}
-//		}
-	
 	for(int j = 0; j < K; ++j)
 		for(int color = 1; color <= M; ++color)
 			preAns[0][j].at(color) = preAns[0][j].at(color - 1) + sum[j].at(color) * sum[j].at(color);
 	
-	
-	freopen("color2.err","w",stderr);
-	
-	
 	for(int k = 1; k < K; ++k)
 		for(int j = k; j < K; ++j)
-			for(int color = 1; color <= M; ++color){
+			for(int color = 1; color <= M; ++color)
 				preAns[k][j].at(color) = preAns[k][j].at(color - 1) + (sum[j][color] - sum[k - 1][color]) * (sum[j][color] - sum[k - 1][color]);
-				debug(k,j,color,sum[j][color] - sum[k - 1][color],preAns[k][j][color],preAns[k][j][color - 1]);
-			}
 			
-	fclose(stderr);
 }
 
 Query decode(Query Object, int lastAns){
@@ -126,7 +105,6 @@ Query decode(Query Object, int lastAns){
 
 valueType query(Query x){
 	int const &l = x.l, &r = x.r, &a = x.a, &b = x.b;
-	debug(l, r, a, b);
 	int ans(0);
 	cnt.fill(0);
 	
@@ -135,16 +113,8 @@ valueType query(Query x){
 			if(a <= source[i] && source[i] <= b)
 				++ans += 2 * cnt[source[i]]++;
 	} else {
-//		int const ansB = (preAns[belong[r] - 1][b] - preAns[belong[l]][b]) * (preAns[belong[r] - 1][b] + preAns[belong[l]][b]);
-//		int const ansA = (preAns[belong[r] - 1][a - 1] - preAns[belong[l]][a - 1]) * (preAns[belong[r] - 1][a - 1] + preAns[belong[l]][a - 1]);
-//		ans = ansB - ansA;
 		ans = preAns[belong[l] + 1][belong[r] - 1][b] - preAns[belong[l] + 1][belong[r] - 1][a - 1];
-		int const preA = preAns[belong[l] + 1][belong[r] - 1][a - 1], preB = preAns[belong[l] + 1][belong[r] - 1][b];
-		debug(ans); 
-		debug(preA,preB);
-		debug(belong[l],belong[r]);
-		for(int c = a; c <= b; ++c)
-			debug(c,sum[belong[r - 1]][c],sum[belong[l]][c]);
+
 		for(int i = l; i < rightBound[belong[l]]; ++i)
 			if(a <= source[i] && source[i] <= b)
 				++ans += 2 * (sum[belong[r] - 1][source[i]] - sum[belong[l]][source[i]] + cnt[source[i]]++);
