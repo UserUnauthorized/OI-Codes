92a18808e0171d795be09f7e6e47c2abd482461e
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-02-11 15:38:10 +0800

    36.9 & P2709 小B的询问 [Half]

--- HZ/36.9.cpp
+++ HZ/36.9.cpp
@@ -2,4 +2,24 @@
 //Luogu - P2709
 #include<bits/stdc++.h>
 using namespace std;
+constexpr int maxN = 5e4 + 5;
+typedef long long valueType;
+array<valueType, maxN> source, belong, cnt;
 
+struct Query{
+	int l;
+	int r;
+	
+	Query():l(-1), r(-1){};
+	Query(int _l_, int _r_):l(_l_), r(_r_){};
+	
+	bool operator<(Query const &Object) const{
+		if(belong[this->l] != belong[Object.l])
+			return this->l < Object.l;
+			
+		if(belong[this->r] != belong[Object.r])
+			return this->r < Object.r;
+			
+		return this->pos < Object.pos;
+	}
+};
