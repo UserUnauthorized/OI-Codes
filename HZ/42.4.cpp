a7e2e355120912464027d763e362f7c9d8da2af6
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-02-12 10:38:42 +0800

    42.4 & P3369 【模板】普通平衡树

--- HZ/42.4.cpp
+++ HZ/42.4.cpp
@@ -1,2 +1,31 @@
 //HZ - 42.4
 //Luogu - P3369
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long valueType;
+constexpr valueType maxN = 1e5 + 5;
+
+struct NODE{
+	typedef NODE self;
+	typedef self* pointer;
+	typedef self& reference;
+
+	pointer father;
+	pointer leftSon;
+	pointer rightSon;
+	valueType value;
+	valueType cnt;
+	valueType size;
+	
+	pointer &son(bool _rightSon_){
+		return _rightSon_ ? this->rightSon : this->leftSon;
+	}
+	
+	bool isRightSon() const {
+		return this == *(this->father)->rightSon;
+	}
+};
+
+class SPLAY{
+	
+};
