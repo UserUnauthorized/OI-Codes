75638aa8bfd7216b818b28e347075ac6890e6553
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-02-12 11:18:17 +0800

    42.4 & P3369 【模板】普通平衡树

--- HZ/42.4.cpp
+++ HZ/42.4.cpp
@@ -14,18 +14,54 @@ struct NODE{
 	pointer leftSon;
 	pointer rightSon;
 	valueType value;
-	valueType cnt;
+	valueType count;
 	valueType size;
 	
-	pointer &son(bool _rightSon_){
+	NODE():father(NULL), leftSon(NULL), rightSon(NULL), value(0), count(0), size(0){};
+	
+	pointer &son(bool _rightSon_) const{
 		return _rightSon_ ? this->rightSon : this->leftSon;
 	}
 	
 	bool isRightSon() const {
-		return this == *(this->father)->rightSon;
+		return this == (this->father)->rightSon;
+	}
+	
+	void update(){
+		this->size = (this->leftSon ? (this->leftSon)->size : 0) + (this->rightSon ? (this->rightSon)->size : 0) + this->cnt;
+	}
+	
+	void init(){
+		this->leftSon = this->rightSon = this->father = NULL;
+		this->value = 0;
+		this->count = this->size = 1;
+	}
+	
+	void clear(){
+		this->leftSon = this->rightSon = this->father = NULL;
+		this->value this->count = this->size = 0;
 	}
 };
 
-class SPLAY{
+struct SPLAY{
+	typedef NODE node;
+	typedef node::pointer pointer;
 	
+	pointer root;
+	
+	SPLAY(): root(NULL){};
+	
+	void rotate(pointer now){
+		pointer father = now->father;
+		bool isRightSon = now->isRightSon();
+		father->son(isRightSon) = now->son(!isRightSon);
+		
+		if(now->son(!isRightSon))
+			(now->son(!isRightSon))->father = father;
+			
+		if(now->father = father->father)
+			(now->father)->son(father->isRightSon()) = now;
+			
+		father->father = now;
+	}
 };
