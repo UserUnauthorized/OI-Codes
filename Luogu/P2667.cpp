adf700235ddb76ebd560c600d690673cf3a6f1f4
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-03-23 18:20:29 +0800

    P2667 [TJOI2012]防御

--- Luogu/P2667.cpp
+++ Luogu/P2667.cpp
@@ -1,34 +1,32 @@
 //Luogu - P2667
 #include<bits/stdc++.h>
-
-typedef unsigned int valueType;
-typedef unsigned int posType;
+typedef long long valueType;
 constexpr valueType MOD = 1e9 + 9, maxN = 1e5 + 5;
 
-struct TREE{
-	posType l, r;
+valueType N_, Q_;
+valueType const &N = N_, &Q = Q_;
+
+struct SEGNODE{
+	int l, r;
 	valueType lazy;
 	valueType remain;
 };
 
-valueType N, Q;
+typedef std::array<valueType, maxN> ARRAY;
+ARRAY source;
 
-std::array<TREE, maxN << 2> tree;
-std::array<valueType, maxN> source;
-
-void build(valueType id, posType l, posType r);
-void arrack(valueType id, posType l, posType r, valueType key);
-valueType query(valueType id, posType pos);
-
-void init();
-
-int main(){
-	init();
-}
-
-void init(){
-	std::cin >> N >> Q;
+namespace before{
+	std::array<::SEGNODE, maxN << 2> tree;
+	ARRAY const &source = ::source;
 	
-	for(int i = 1; i <= N; ++i)
-		std::cin >> source[i];
+	void build(int id, int l, int r){
+		tree[id].l = l;
+		tree[id].r = r;
+		
+		if(l == r){
+			tree[id].remain = 
+		}
+		
+		int const mid = (l + r) >> 1;
+	}
 }
