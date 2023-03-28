e15de12db7913a35cc93ad69db3cb4c5015ed690
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-03-28 10:35:45 +0800

    26.2 & P2319 [HNOI2006]超级英雄

---
+++ Luogu/P2319.cpp
@@ -0,0 +1,72 @@
+//Luogu - P2319
+#include<bits/stdc++.h>
+
+constexpr int maxN = 1005;
+constexpr int maxM = 1005;
+
+typedef std::list<int> LIST;
+
+int N_, M_;
+int const &N = N_, &M = M_;
+
+std::array<LIST, maxN> edge;
+std::array<int, maxN> match, use;
+std::bitset<maxN> visited;
+
+bool dfs(int x);
+
+void init();
+
+int main(){
+	init();
+	
+	int ans = 0;
+	
+	for(int i = 1; i <= M; ++i){
+		visited.reset();
+		
+		if(dfs(i))
+			++ans;
+		else
+			break;
+	}
+	
+	std::cout << ans << '\n';
+	
+	for(int i = 1; i <= ans; ++i)
+		std::cout << use[i] - 1 << '\n';
+	
+	return 0;
+}
+
+void init(){
+	std::cin >> N_ >> M_;
+	
+	for(int i = 1; i <= M; ++i){
+		int x, y;
+		
+		std::cin >> x >> y;
+		
+		++x;
+		++y;
+		
+		edge[i].emplace_back(x);
+		edge[i].emplace_back(y);
+	}
+}
+
+bool dfs(int x){
+	for(auto iter : edge[x]){
+		if(!visited[iter]){
+			visited[iter] = true;
+			
+			if(!match[iter] || dfs(match[iter])){
+				match[iter] = x;
+				use[x] = iter;
+				return true;
+			}
+		}
+	}
+	
+	return false;
+}
