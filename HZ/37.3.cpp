4dacd6d387a58ba6aedb895e2d6cc48d7e5a98dd
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-04-06 21:59:42 +0800

    update

---
+++ HZ/37.3.cpp
@@ -0,0 +1,41 @@
+//HZ - 37.3
+//Luogu - P4591
+
+#include<bits/stdc++.h>
+
+typedef unsigned long long hashType;
+typedef long long valueType;
+constexpr valueType maxK = 103, maxS = 1e4 + 5, maxA = 12;
+constexpr hashType base = 1331;
+
+valueType K_;
+
+valueType const &K = K_;
+
+std::array<std::array<valueType, maxS>, maxK> dp;
+
+std::array<std::array<hashType, maxA>, maxK> subHash;
+
+std::array<std::array<std::string, maxA>, maxK> subStr;
+
+std::string source;
+
+std::array<hashType, maxS> sourceHash;
+
+std::array<int, maxK> size;
+
+hashType table(char ch);
+
+void init();
+
+int main() {
+	init();
+}
+
+void init() {
+	
+}
+
+hashType table(char ch){
+	return ch - 'a' + 1;
+}
