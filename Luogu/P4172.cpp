3485fd88192d19e397bdf8ce97a5be654b02afbc
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-04-19 16:37:41 +0800

    80.4 & P4172 [WC2006]水管局长

--- Luogu/P4172.cpp
+++ Luogu/P4172.cpp
@@ -2,6 +2,46 @@
 //Luogu - P4172
 #include<bits/stdc++.h>
 
+namespace DEBUG {
+    template<typename T>
+    inline void _debug(const char *format, T t) {
+        std::cerr << format << '=' << t << std::endl;
+    }
+
+    template<class First, class... Rest>
+    inline void _debug(const char *format, First first, Rest... rest) {
+        while (*format != ',') std::cerr << *format++;
+        std::cerr << '=' << first << ",";
+        _debug(format + 1, rest...);
+    }
+
+    template<typename T>
+    std::ostream &operator<<(std::ostream &os, const std::vector<T> &V) {
+        os << "[ ";
+        for (const auto &vv: V) os << vv << ", ";
+        os << "]";
+        return os;
+    }
+    
+    std::ostream &operator<<(std::ostream &os, __int128 V) {
+        if(V < 0){
+        	os << '-';
+        	V = -V;
+		}
+		
+		if(V > 9)	
+			os << V / 10;
+		
+		os << (int)(V % 10);
+		
+        return os;
+    }
+
+#define debug(...) _debug(#__VA_ARGS__, __VA_ARGS__)
+}  // namespace DEBUG
+
+using namespace DEBUG;
+
 typedef int valueType;
 
 constexpr valueType MIN = INT_MIN >> 1;
@@ -372,8 +412,11 @@ int main() {
 	std::stack<int> ans;
 	
 	for(int i = Q; i > 0; --i) {
+		debug(i, oper[i].k, oper[i].a, oper[i].b);
 		if(oper[i].k == 1) {
-			ans.push(edge[tree.ans(oper[i].a, oper[i].b)].first);
+			int const pos = tree.ans(oper[i].a, oper[i].b) - N - 1;
+			
+			ans.push(edge[tree.ans(oper[i].a, oper[i].b) - N - 1].first);
 		} else {
 			int const pre = tree.ans(oper[i].a, oper[i].b);
 			
@@ -387,7 +430,6 @@ int main() {
 			}
 		}
 	}
-	
 	while(!ans.empty()) {
 		std::cout << ans.top() << '\n';
 		ans.pop();
