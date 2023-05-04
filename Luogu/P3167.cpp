f3fe74f6bda10827c664ad6f74decf2f3cf81e29
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-05-04 10:50:44 +0800

    P3167 & 37.4

--- Luogu/P3167.cpp
+++ Luogu/P3167.cpp
@@ -2,85 +2,99 @@
 //Luogu - P3167
 #include <bits/stdc++.h>
 
-using namespace std;
-constexpr int maxW = 1e5 + 5, base = 27;
-typedef array<char, maxW> inputStr;
-typedef unsigned long long hash_type;
+typedef std::array<long long, 256> tableType;
 
-void init();
+constexpr static const tableType HASH_TABLE = {0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 1, 2, 3, 4, 5, 6,
+                                               7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+                                               17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+                                               0, 0, 0, 0, 0, 0, 1, 2, 3, 4,
+                                               5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+                                               15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+                                               25, 26, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, };
 
-bool check(const inputStr &str, int length);
+constexpr static const size_t HASH_CNT = 3;
+constexpr static const std::array<long long, HASH_CNT> HASH_MOD = {1000000007, 1000000009, 1000000021};
+constexpr static const std::array<long long, HASH_CNT> HASH_BASE = {31, 1331, 87481};
 
-bool check(int strPos, int matchPos, const inputStr &str, const array<hash_type, maxW> &strHash);
+class stringHash {
+public:
+    typedef std::string dataType;
+    typedef long long hashType;
+    typedef size_t sizeType;
+    typedef std::vector<hashType> hashListType;
 
-inline bool isWild(const char &c);
+protected:
+    dataType data;
+    std::array<hashListType, HASH_CNT> hash;
+    std::array<hashListType, HASH_CNT> pow;
 
-inline bool isStr(const char &c);
+public:
+    stringHash() = default;
 
-array<hash_type, maxW> matchHash;
-array<hash_type, maxW> bin;
-inputStr match;
-array<int, maxW> subLength;
-int matchLength, n;
+    explicit stringHash(dataType _data_) : data(std::move(_data_)) {
+        sizeType const size = data.size();
 
-int main() {
-    init();
+        for (sizeType i = 0; i < HASH_CNT; ++i) {
+            pow[i].resize(size + 1);
+            pow[i][0] = 1;
 
-    while (n--) {
-        inputStr input;
-        int inputLength(0);
-        int i(getchar());
-        while (isStr((char) i) || isWild((char) i)) {
-            input[++inputLength] = (char) i;
-            i = getchar();
+            for (sizeType j = 0; j < size; ++j)
+                pow[i][j + 1] = pow[i][j] * HASH_BASE[i] % HASH_MOD[i];
         }
-        cout << (check(input, inputLength) ? "YES" : "NO");
+
+        for (sizeType i = 0; i < HASH_CNT; ++i) {
+            hash[i].resize(size + 1);
+            hash[i][0] = 0;
+            for (sizeType j = 0; j < size; ++j) {
+                hash[i][j + 1] = (hash[i][j] * HASH_BASE[i] + HASH_TABLE[data[j]]) % HASH_MOD[i];
+            }
+        }
     }
-}
 
-void init() {
-    int input(getchar());
-    while (isStr((char) input) || isWild((char) input)) {
-        match[++matchLength] = (char) input;
-        input = getchar();
+    hashType getSubHash(sizeType i) const {
+        return hash[i][this->size()];
     }
 
-    pair<int, int> continuous;
-
-    for (int i = 1; i <= matchLength; ++i)
-        isWild(match[i]) ? (subLength[continuous.first] = continuous.second, continuous.first =
-                i + 1, continuous.second = 0, matchHash[i] *= base) : (++continuous.second,
-                matchHash[i] = matchHash[i - 1] * base + match[i] - 'a' + 1);
-    subLength[continuous.first] = continuous.second;
-
-    cin >> ::n;
-
-    bin[0] = 1;
-    for (int i = 1; i <= maxW; ++i)
-        bin[i] = bin[i - 1] * base;
-}
-
-bool isWild(const char &c) {
-    return (c == '?' || c == '*');
-}
-
-bool isStr(const char &c) {
-    return (c >= 'a' && c <= 'z');
-}
-
-bool check(const inputStr &str, int length) {
-    array<hash_type, maxW> strHash{};
-
-    for (int i = 1; i <= length; ++i)
-        strHash[i] = strHash[i - 1] * base + str[i] - 'a' + 1;
-
-    for (int i = 1; i <= matchLength; ++i) {
-
+    hashType getSubHash(sizeType l, sizeType r, sizeType i) const {
+//        assert(0 < l && l <= r && r <= this->size());
+        return (hash[i][r] - hash[i][l - 1] * pow[i][r - l + 1] % HASH_MOD[i] + HASH_MOD[i]) % HASH_MOD[i];
     }
-}
 
-bool check(int strPos, int matchPos, const inputStr &str, const array<hash_type, maxW> &strHash) {
-    if (isWild(match[matchPos])) {
-        for (int i = 1)
+    sizeType size() const {
+        return data.size();
     }
-}
+
+    bool check(sizeType pos, const stringHash &str) const {
+        sizeType const size = str.size();
+
+        if (pos + size - 1 > this->size())
+            return false;
+
+        for (sizeType i = 0; i < HASH_CNT; ++i) {
+            if (getSubHash(pos, pos + size - 1, i) != str.getSubHash(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
