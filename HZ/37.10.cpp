a74d6f0f0f60dce1a1eaa499ed3afcbd6bfc034f
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-05-15 18:56:36 +0800

    37.10 BZOJ1461字符串的匹配

--- HZ/37.10.cpp
+++ HZ/37.10.cpp
@@ -1,18 +1,43 @@
 //HZ - 37.10
 #include<bits/stdc++.h>
 
-constexpr int maxN = 5e5 + 5, maxS = 1e4 + 5;
+constexpr int maxN = 5e5 + 5, maxK = maxN, maxS = 1e4 + 5;
 
 enum LimitEnum : int {
-	EMPTY = -1;
-	EQUAL = 1;
-	LOWER = 2;
-	UPPER = 3;
+	EMPTY = -1,
+	EQUAL = 1,
+	LOWER = 2,
+	UPPER = 3
 };
 
 typedef std::pair<LimitEnum, int> LimitType;
 
-typedef std::array<LimitType, 2> LimitSet;
+typedef std::vector<LimitType> LimitSet;
 
-std::array<LimitSet, maxS> limit;
-std::array<>
+int N_, K_, S_;
+int const &N = N_, &K = K_, &S = S_;
+
+int main() {
+	std::cin >> N_ >> K_ >> S_;
+	
+	int const textLength = N, patternLength = K;
+	
+	std::vector<int> sortedPattern, patternRank(patternLength, -1), lastPos(S + 1, -1), text(textLength), pattern(patternLength);
+	
+	for(int i = 0; i < textLength; ++i)
+		std::cin >> text[i];
+		
+	for(int i = 0; i < patternLength; ++i)
+		std::cin >> pattern[i];
+		
+	sortedPattern.assign(pattern.begin(), pattern.end());
+
+	std::sort(sortedPattern.begin(), sortedPattern.end());
+	
+	for(int i = 0; i < patternLength; ++i)
+		patternRank[i] = std::distance(sortedPattern.begin(), std::lower_bound(sortedPattern.begin(), sortedPattern.end(), pattern[i])) + 1;
+		
+	for(int i = 1; i < patternLength; ++i) {
+		if(lastPos[pattern[i]])
+	}
+}
