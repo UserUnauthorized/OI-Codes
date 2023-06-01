e838776253e61b8e3a8b3c269a3db565389ddfde
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-06-01 11:50:04 +0800

    29.9 & P2155 [SDOI2008] 沙拉公主的困惑

---
+++ HZ/35.18.cpp
@@ -0,0 +1,10 @@
+//HZ - 35.18
+#include<bits/stdc++.h>
+
+typedef long long valueType;
+typedef size_t sizeType;
+typedef long double realType;
+typedef std::vector<realType> realVector;
+typedef std::vector<valueType> valueVector;
+
+
