95720089271dbca114fe0363024c844b0749cc12
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-06-06 19:24:47 +0800

    P1806 跑步

---
+++ Luogu/P3808.cpp
@@ -0,0 +1,18 @@
+//Luogu - P3808
+#include<bits/stdc++.h>
+
+class AC {
+public:
+	typedef int valueType;
+	typedef size_t sizeType;
+	typedef std::function<sizeType(char)> Converter;
+	static sizeType const charSetSize = 10;
+	
+private:
+	struct NODE{
+		typedef NODE self;
+		typedef 
+		
+		valueType value;
+	};
+};
