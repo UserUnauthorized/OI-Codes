434eedd9417e2e81776d7b495ab37c731b3e98b4
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-06-08 18:26:13 +0800

    P2605 [ZJOI2010]基站选址

--- Luogu/P2605.cpp
+++ Luogu/P2605.cpp
@@ -5,6 +5,7 @@
 typedef int valueType;
 typedef size_t sizeType;
 typedef std::vector<valueType> ValueVector;
+typedef std::vector<ValueVector> ValueMatrix;
 typedef std::vector<sizeType> SizeVector;
 
 constexpr valueType MIN = INT_MIN >> 1, MAX = INT_MAX >> 1;
@@ -15,7 +16,7 @@ valueType const &N = N_, &K = K_;
 ValueVector D_, C_, S_, W_;
 ValueVector const &D = D_, &C = C_, &S = S_, &W = W_;
 
-class LazyArray{
+class MinArray{
 private:
 	sizeType size;
 	
@@ -24,13 +25,15 @@ private:
 	ValueVector data;
 	SizeVector belong;
 	
-	ValueVector tag;
+	ValueVector tag, min;
 	SizeVector leftBound, rightBound;
 	
-public:
-	LazyArray() = default;
+	valueType result;
 	
-	LazyArray(sizeType N) : size(N), blockSize(std::ceil(std::sqrt(size))), blockCount(std::ceil((double)size / (double)blockSize)),
+public:
+	MinArray() = default;
+	
+	MinArray(sizeType N) : size(N), blockSize(std::ceil(std::sqrt(size))), blockCount(std::ceil((double)size / (double)blockSize)),
 			data(size, 0), belong(size, 0), tag(blockCount, 0), leftBound(blockCount, 0), rightBound(blockCount, 0) {
 		for(sizeType i = 0; i < N; ++i)
 			belong[i] = i / blockSize;
@@ -46,11 +49,13 @@ public:
 		size = N;
 		blockSize = std::ceil(std::sqrt(size));
 		blockCount = std::ceil((double)size / (double)blockSize);
-		data.resize(N, 0);
+		data.resize(N, MAX);
 		belong.resize(N, 0);
 		tag.resize(blockCount, 0);
+		min.resize(blockCount, 0);
 		leftBound.resize(blockCount, 0);
 		rightBound.resize(blockCount, 0);
+		result = MAX;
 	}
 	
 	void update(sizeType l, sizeType r, valueType value) {
@@ -63,32 +68,67 @@ public:
 		if(left == right) {
 			for(sizeType i = l; i <= r; ++i)
 				data[i] += value;
+			
+			min[left] = MAX;
+			
+			for(sizeType i = leftBound[left]; i < rightBound[left]; ++i)
+				min[left] = std::min(min[left], data[i]);
 		} else {
-			for(sizeType i = left + 1; i < right; ++i)
+			for(sizeType i = left + 1; i < right; ++i) {
 				tag[i] += value;
+				min[i] += value;
+			}
 		
 			for(sizeType i = l; i < rightBound[left]; ++i)
 				data[i] += value;
 			
 			for(sizeType i = leftBound[right]; i <= r; ++i)
 				data[i] += value;
+			
+			min[left] = MAX;	
+			for(sizeType i = leftBound[left]; i < rightBound[left]; ++i)
+				min[left] = std::min(min[left], data[i]);
+			
+			min[right] = MAX;	
+			for(sizeType i = leftBound[right]; i < rightBound[right]; ++i)
+				min[right] = std::min(min[right], data[i]);
 		}
 	}
 	
-	const valueType &operator[](sizeType i) const {
+	void set(sizeType pos, valueType key) {
+		data[pos] = key - tag[belong[pos]];
+		
+		min[belong[pos]] = std::min(tag[belong[pos]], data[pos]);
+	}
+	
+	valueType ans() const {
+		valueType result = MAX;
+		
+		for(sizeType i = 0; i < blockCount; ++i)
+			result = std::min(result, min[i]);
+			
+		return result;
+	}
+	
+	valueType operator[](sizeType i) const {
 		return data[i] + tag[belong[i]];
 	}
-} Lazy;
+};
+
+std::vector<MinArray> minValue;
 
 void offline(valueType id) {
 	valueType const leftPos = D[id] - S[id];
 	
 	sizeType const leftId = std::distance(D.begin(), std::lower_bound(D.begin(), D.end(), leftPos));
 	
-	if(leftId > 1)
-		Lazy.update(1, leftId - 1, W[id]);
+	if(leftId > 1) {
+		for(valueType i = 1; i <= K; ++i)
+			minValue[i].update(1, leftId - 1, W[id]);
+	}
 		
-	Lazy.update(id + 1, N, W[id]);
+	for(sizeType i = 1; i <= K; ++i)
+		minValue[i].update(id + 1, N, W[id]);	
 }
 
 class DistanceArray {
@@ -170,7 +210,9 @@ public:
 int main() {
 	std::cin >> N_ >> K_;
 	
-	pay.init(N + 1);
+	minValue.resize(K + 1);
+	for(sizeType i = 0; i <= K; ++i)
+		minValue[i].init(N + 1);
 	
 	distance.init(N + 1);
 	
@@ -190,19 +232,22 @@ int main() {
 	for(int i = 1; i <= N; ++i)
 		std::cin >> W_[i];
 		
-	std::vector<ValueVector, N + 1> dp;
+	std::array<ValueVector, 2> dp;
 	
-	for(int i = 1; <= N; ++i)
-		dp[i].resize(K + 1, MAX);
+	dp[0].resize(K + 1, MAX);
+	dp[1].resize(K + 1, MAX);
 	
-	dp[1][1] = C[i];
+	dp[1][1] = C[1];
 	
-	dp[1][0] = S[i];
+	dp[1][0] = W[1];
+	
+	minValue[1].set(1, C[1]);
+	minValue[0].set(1, W[1]);
 	
 	for(int i = 2; i <= N; ++i) {
 		sizeType const now = i & 1, pre = now ^ 1;
 		
-		dp[now][0] = dp[pre][0] + S[i];
+		dp[now][0] = dp[pre][0] + W[i];
 		
 		valueType const step = D[i] - D[i - 1];
 		
@@ -210,6 +255,20 @@ int main() {
 		
 		distance.push(step);
 		
-		for(int j)
+		for(valueType j = 1; j <= K; ++j)
+			dp[now][i] = minValue[j - 1].ans() + C[i];
+		
+		for(valueType j = 1; j <= K; ++j)
+			minValue[j].set(i, dp[now][i]);	
+		
 	}
+
+	valueType ans = MAX;
+	
+	for(int i = 0; i <= K; ++i)
+		ans = std::min(ans, dp[N & 1][i]);
+		
+	std::cout << ans << std::flush;
+	
+	return 0;
 }
