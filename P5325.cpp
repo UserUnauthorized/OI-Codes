a84955110e71db35e9e7e621c93c51b0fdfbbfc2
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-07-29 21:02:25 +0800

    P5325 【模板】Min_25筛

--- P5325.cpp
+++ P5325.cpp
@@ -1,7 +1,6 @@
 //Luogu - P5325
 #include <bits/stdc++.h>
 
-
 typedef long long valueType;
 typedef std::vector<valueType> ValueVector;
 
@@ -35,12 +34,12 @@ T1 sub(const T1 &a, const T2 &b) {
 
 template<typename T1, typename T2>
 T1 mul(const T1 &a, const T2 &b) {
-    return (long long) a * b % MOD;
+    return (long long) (a % MOD) * (b % MOD) % MOD;
 }
 
 template<typename T1, typename T2>
 void Mul(T1 &a, const T2 &b) {
-    a = (long long) a * b % MOD;
+    a = (long long) (a % MOD) * (b % MOD) % MOD;
 }
 
 template<typename T1, typename T2>
@@ -87,7 +86,7 @@ public:
     };
 
     valueType size() const {
-        return (valueType) data.size();
+        return (valueType) data.size() - 1;
     };
 };
 
@@ -102,7 +101,7 @@ private:
 
     valueType size, primeCount;
     container prime, primeSum1, primeSum2, lpf;
-    container G_0, G_1, F_prime;
+    container G_1, G_2, F_prime;
 
     void sieve(valueType n) {
         prime.push_back(0);
@@ -110,18 +109,18 @@ private:
         primeSum2.push_back(0);
 
         for (valueType i = 2; i <= n; ++i) {
-            if (!lpf[i]) {
+            if (lpf[i] == 0) {
                 prime.emplace_back(i);
                 primeSum1.emplace_back(sum(primeSum1.back(), i));
                 primeSum2.emplace_back(sum(primeSum2.back(), mul(i, i)));
                 lpf[i] = i;
             }
 
-            for (auto const &iter: prime) {
-                lpf[i * iter] = iter;
-
-                if (i % iter == 0)
+            for (auto iter = prime.begin() + 1; iter != prime.end(); ++iter) {
+                if (*iter > lpf[i] || i * *iter > n)
                     break;
+
+                lpf[i * *iter] = *iter;
             }
         }
 
@@ -129,53 +128,53 @@ private:
     }
 
     static valueType sum1(valueType n) {
-        return n * (n + 1) / 2 % MOD;
+        static valueType constexpr Inv2 = 500000004;
+
+        return mul(mul(n, n + 1), Inv2);
     }
 
     static valueType sum2(valueType n) {
         static valueType constexpr Inv6 = 166666668;
 
-        return n * (n + 1) % MOD * (2 * n + 1) % MOD * Inv6 % MOD;
+        return mul(mul(n, n + 1), mul(2 * n + 1, Inv6));
     }
 
 public:
     explicit Min_25(valueType n) : N(n), limit(std::floor(std::sqrt((long double) N))), ID(n), size(ID.size()),
-                                   primeCount(0), lpf(size + 1, 0), G_0(size + 1, 0), G_1(size + 1, 0),
+                                   primeCount(0), lpf(size + 1, 0), G_1(size + 1, 0), G_2(size + 1, 0),
                                    F_prime(size + 1, 0) {
-        valueType count = 0; // count of primes
-
         sieve(limit);
 
-        for (valueType i = 1; i <= size; ++i) {
-            G_0[i] = sum1(i);
-            G_1[i] = sum2(i);
+        for (valueType i = 1; i < size; ++i) {
+            G_1[i] = sum1(ID[i]);
+            G_2[i] = sum2(ID[i]);
         }
 
         //calc F_prime
         for (valueType k = 1; k <= primeCount; ++k) {
-            valueType p = prime[k];
+            valueType const p = prime[k];
             valueType const p2 = p * p;
 
             for (valueType i = 1; ID[i] >= p2; ++i) {
                 valueType const id = ID(ID[i] / p);
 
-                Dec(G_0[i], mul(p, sub(G_0[id], primeSum1[k - 1])));
-                Dec(G_0[i], mul(mul(p, p), sub(G_0[id], primeSum2[k - 1])));
+                Dec(G_1[i], mul(p, sub(G_1[id], primeSum1[k - 1] + 1)));
+                Dec(G_2[i], mul(mul(p, p), sub(G_2[id], primeSum2[k - 1] + 1)));
             }
         }
 
         for (valueType i = 1; i <= size; ++i)
-            F_prime[i] = sub(G_1[i], G_0[i]);
+            F_prime[i] = sub(G_2[i], G_1[i]);
     }
 
 private:
-    valueType F_p(valueType n) {
+    static valueType F_p(valueType n) {
         return mul(n, sub(n, 1));
     }
 
 public:
     valueType F(valueType k, valueType n) {
-        if (n < prime[k] || n <= 1)
+        if ((k < prime.size() && n < prime[k]) || n <= 1)
             return 0;
 
         valueType const id = ID(n);
@@ -183,10 +182,10 @@ public:
         valueType result = sub(F_prime[id], sub(primeSum2[k - 1], primeSum1[k - 1]));
 
         for (valueType i = k; i <= primeCount && prime[i] * prime[i] <= n; ++i) {
-            valueType p = prime[i];
+            valueType const p = prime[i];
 
-            for (valueType c = 1, p_c = p; p_c <= n; ++c, p_c *= p)
-                Inc(result, sum(mul(F_p(p_c), F(i + 1, n / p_c)), F_p(p_c * p)));
+            for (valueType p1 = p, p2 = p * p; p2 <= n; p1 *= p, p2 *= p)
+                Inc(result, sum(mul(F_p(p1), F(i + 1, n / p1)), F_p(p2)));
         }
 
         return result;
