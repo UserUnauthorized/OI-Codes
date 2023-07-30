335f1410c4e9ddeb708078290a5a482e17fd9eb3
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-07-30 21:25:42 +0800

    P2472 [SCOI2007] 蜥蜴

--- Luogu/P2472.cpp
+++ Luogu/P2472.cpp
@@ -2,45 +2,6 @@
 //Luogu - P2472
 #include <bits/stdc++.h>
 
-namespace DEBUG {
-    template<typename T>
-    inline void _debug(const char *format, T t) {
-        std::cerr << format << '=' << t << std::endl;
-    }
-
-    template<class First, class... Rest>
-    inline void _debug(const char *format, First first, Rest... rest) {
-        while (*format != ',') std::cerr << *format++;
-        std::cerr << '=' << first << ",";
-        _debug(format + 1, rest...);
-    }
-
-    template<typename T>
-    std::ostream &operator<<(std::ostream &os, const std::vector<T> &V) {
-        os << "[ ";
-        for (const auto &vv: V) os << vv << ", ";
-        os << "]";
-        return os;
-    }
-
-    std::ostream &operator<<(std::ostream &os, __int128 V) {
-        if (V < 0) {
-            os << '-';
-            V = -V;
-        }
-
-        if (V > 9)
-            os << V / 10;
-
-        os << (int) (V % 10);
-
-        return os;
-    }
-
-#define debug(...) _debug(#__VA_ARGS__, __VA_ARGS__)
-}  // namespace DEBUG
-
-using namespace DEBUG;
 typedef long long valueType;
 typedef std::vector<valueType> ValueVector;
 
