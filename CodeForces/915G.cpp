fd1dcd2240b141d3ba1f50eaa00eb0d9c3c43a67
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-09-01 21:35:22 +0800

    CF915G Coprime Arrays

---
+++ CodeForces/915G.cpp
@@ -0,0 +1,235 @@
+//Codeforces - 915G
+#include <bits/stdc++.h>
+
+typedef long long valueType;
+typedef std::vector<valueType> ValueVector;
+typedef std::vector<ValueVector> ValueMatrix;
+typedef std::vector<bool> bitset;
+
+constexpr valueType MOD = 1e9 + 7;
+
+
+#define ModOperSafeModOption false
+
+template<typename T1, typename T2, typename T3 = valueType>
+void Inc(T1 &a, T2 b, const T3 &mod = MOD) {
+#if ModOperSafeModOption
+    a %= mod;
+    b %= mod;
+
+    if (a < 0)
+        a += mod;
+
+    if (b < 0)
+        b += mod;
+#endif // ModOperSafeModOption
+
+    a = a + b;
+
+    if (a >= mod)
+        a -= mod;
+}
+
+template<typename T1, typename T2, typename T3 = valueType>
+void Dec(T1 &a, T2 b, const T3 &mod = MOD) {
+#if ModOperSafeModOption
+    a %= mod;
+    b %= mod;
+
+    if (a < 0)
+        a += mod;
+
+    if (b < 0)
+        b += mod;
+#endif // ModOperSafeModOption
+
+    a = a - b;
+
+    if (a < 0)
+        a += mod;
+}
+
+template<typename T1, typename T2, typename T3 = valueType>
+T1 sum(T1 a, T2 b, const T3 &mod = MOD) {
+#if ModOperSafeModOption
+    a %= mod;
+    b %= mod;
+
+    if (a < 0)
+        a += mod;
+
+    if (b < 0)
+        b += mod;
+#endif // ModOperSafeModOption
+
+    return a + b >= mod ? a + b - mod : a + b;
+}
+
+template<typename T1, typename T2, typename T3 = valueType>
+T1 sub(T1 a, T2 b, const T3 &mod = MOD) {
+#if ModOperSafeModOption
+    a %= mod;
+    b %= mod;
+
+    if (a < 0)
+        a += mod;
+
+    if (b < 0)
+        b += mod;
+#endif // ModOperSafeModOption
+
+    return a - b < 0 ? a - b + mod : a - b;
+}
+
+template<typename T1, typename T2, typename T3 = valueType>
+T1 mul(T1 a, T2 b, const T3 &mod = MOD) {
+#if ModOperSafeModOption
+    a %= mod;
+    b %= mod;
+
+    if (a < 0)
+        a += mod;
+
+    if (b < 0)
+        b += mod;
+#endif // ModOperSafeModOption
+
+    return (long long) a * b % mod;
+}
+
+template<typename T1, typename T2, typename T3 = valueType>
+void Mul(T1 &a, T2 b, const T3 &mod = MOD) {
+#if ModOperSafeModOption
+    a %= mod;
+    b %= mod;
+
+    if (a < 0)
+        a += mod;
+
+    if (b < 0)
+        b += mod;
+#endif // ModOperSafeModOption
+
+    a = (long long) a * b % mod;
+}
+
+template<typename T1, typename T2, typename T3 = valueType>
+T1 pow(T1 a, T2 b, const T3 &mod = MOD) {
+#if ModOperSafeModOption
+    a %= mod;
+    b %= mod;
+
+    if (a < 0)
+        a += mod;
+
+    if (b < 0)
+        b += mod;
+#endif // ModOperSafeModOption
+
+    T1 result = 1;
+
+    while (b > 0) {
+        if (b & 1)
+            Mul(result, a, mod);
+
+        Mul(a, a, mod);
+        b = b >> 1;
+    }
+
+    return result;
+}
+
+class LineSieve {
+public:
+    typedef std::vector<valueType> container;
+
+private:
+    valueType N, M;
+    container prime;
+    bitset isPrime;
+    container Mobius, Power;
+    ValueMatrix Factor;
+
+public:
+    LineSieve(valueType n, valueType m) : N(n), M(m), prime(), isPrime(n + 1, true), Mobius(n + 1, 1), Factor(n + 1),
+                                          Power(n + 1, 1) {
+        isPrime[0] = isPrime[1] = false;
+        Mobius[1] = 1;
+        Power[1] = 1;
+
+        for (valueType i = 2; i <= N; ++i) {
+            if (isPrime[i]) {
+                prime.push_back(i);
+
+                Mobius[i] = -1;
+
+                Power[i] = pow(i, M);
+            }
+
+            for (auto const &iter: prime) {
+                valueType const t = i * iter;
+
+                if (t > N)
+                    break;
+
+                isPrime[t] = false;
+
+                Power[t] = mul(Power[i], Power[iter]);
+
+                if (i % iter == 0) {
+                    Mobius[t] = 0;
+
+                    break;
+                } else {
+                    Mobius[t] = -Mobius[i];
+                }
+            }
+        }
+
+        for (valueType i = 1; i <= N; ++i)
+            for (valueType j = i; j <= N; j += i)
+                Factor[j].push_back(i);
+    }
+
+    valueType operator()(valueType n) const {
+        return Mobius[n];
+    }
+
+    valueType pow(valueType n) const {
+        return Power[n];
+    }
+
+    const ValueVector &fact(valueType n) const {
+        return Factor[n];
+    }
+};
+
+int main() {
+    valueType N, K;
+
+    std::cin >> N >> K;
+
+    LineSieve const sieve(K, N);
+
+    ValueVector ans(K + 1, 0), F(K + 1, 0);
+
+    for (valueType i = 1; i <= K; ++i) {
+        ans[i] = ans[i - 1];
+
+        for (auto const &iter: sieve.fact(i)) {
+            Inc(ans[i], sub(sieve.pow(i / iter), sieve.pow(i / iter - 1)));
+
+            Mul(F[i / iter], i / iter);
+        }
+
+        F[i] = sieve(i);
+
+        Inc(ans[i], F[i]);
+    }
+
+    valueType result = 0;
+
+    for (valueType i = 1; i <= K; ++i) {
+
+    }
+}
