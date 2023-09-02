27c889b286d35e675cf90b466ecfefd709119502
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-09-02 10:13:17 +0800

    CF915G Coprime Arrays

--- CodeForces/915G.cpp
+++ CodeForces/915G.cpp
@@ -8,22 +8,8 @@ typedef std::vector<bool> bitset;
 
 constexpr valueType MOD = 1e9 + 7;
 
-
-#define ModOperSafeModOption false
-
 template<typename T1, typename T2, typename T3 = valueType>
 void Inc(T1 &a, T2 b, const T3 &mod = MOD) {
-#if ModOperSafeModOption
-    a %= mod;
-    b %= mod;
-
-    if (a < 0)
-        a += mod;
-
-    if (b < 0)
-        b += mod;
-#endif // ModOperSafeModOption
-
     a = a + b;
 
     if (a >= mod)
@@ -31,101 +17,22 @@ void Inc(T1 &a, T2 b, const T3 &mod = MOD) {
 }
 
 template<typename T1, typename T2, typename T3 = valueType>
-void Dec(T1 &a, T2 b, const T3 &mod = MOD) {
-#if ModOperSafeModOption
-    a %= mod;
-    b %= mod;
-
-    if (a < 0)
-        a += mod;
-
-    if (b < 0)
-        b += mod;
-#endif // ModOperSafeModOption
-
-    a = a - b;
-
-    if (a < 0)
-        a += mod;
-}
-
-template<typename T1, typename T2, typename T3 = valueType>
-T1 sum(T1 a, T2 b, const T3 &mod = MOD) {
-#if ModOperSafeModOption
-    a %= mod;
-    b %= mod;
-
-    if (a < 0)
-        a += mod;
-
-    if (b < 0)
-        b += mod;
-#endif // ModOperSafeModOption
-
-    return a + b >= mod ? a + b - mod : a + b;
-}
-
-template<typename T1, typename T2, typename T3 = valueType>
 T1 sub(T1 a, T2 b, const T3 &mod = MOD) {
-#if ModOperSafeModOption
-    a %= mod;
-    b %= mod;
-
-    if (a < 0)
-        a += mod;
-
-    if (b < 0)
-        b += mod;
-#endif // ModOperSafeModOption
-
     return a - b < 0 ? a - b + mod : a - b;
 }
 
 template<typename T1, typename T2, typename T3 = valueType>
 T1 mul(T1 a, T2 b, const T3 &mod = MOD) {
-#if ModOperSafeModOption
-    a %= mod;
-    b %= mod;
-
-    if (a < 0)
-        a += mod;
-
-    if (b < 0)
-        b += mod;
-#endif // ModOperSafeModOption
-
     return (long long) a * b % mod;
 }
 
 template<typename T1, typename T2, typename T3 = valueType>
 void Mul(T1 &a, T2 b, const T3 &mod = MOD) {
-#if ModOperSafeModOption
-    a %= mod;
-    b %= mod;
-
-    if (a < 0)
-        a += mod;
-
-    if (b < 0)
-        b += mod;
-#endif // ModOperSafeModOption
-
     a = (long long) a * b % mod;
 }
 
 template<typename T1, typename T2, typename T3 = valueType>
 T1 pow(T1 a, T2 b, const T3 &mod = MOD) {
-#if ModOperSafeModOption
-    a %= mod;
-    b %= mod;
-
-    if (a < 0)
-        a += mod;
-
-    if (b < 0)
-        b += mod;
-#endif // ModOperSafeModOption
-
     T1 result = 1;
 
     while (b > 0) {
