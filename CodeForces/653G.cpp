6efa39b0d1fffa18bc778e415d50c49605b67972
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-09-05 09:09:27 +0800

    CF653G Move by Prime

--- CodeForces/653G.cpp
+++ CodeForces/653G.cpp
@@ -1,27 +1,14 @@
 //Codeforces - 653G
 #include <bits/stdc++.h>
 
-typedef int valueType;
+typedef long long valueType;
 typedef std::vector<valueType> ValueVector;
 typedef std::vector<bool> bitset;
 
 constexpr valueType MOD = 1e9 + 7;
 
-#define ModOperSafeModOption false
-
 template<typename T1, typename T2, typename T3 = valueType>
 void Inc(T1 &a, T2 b, const T3 &mod = MOD) {
-#if ModOperSafeModOption
-    a %= mod;
-    b %= mod;
-
-    if (a < 0)
-        a += mod;
-
-    if (b < 0)
-        b += mod;
-#endif // ModOperSafeModOption
-
     a = a + b;
 
     if (a >= mod)
@@ -30,17 +17,6 @@ void Inc(T1 &a, T2 b, const T3 &mod = MOD) {
 
 template<typename T1, typename T2, typename T3 = valueType>
 void Dec(T1 &a, T2 b, const T3 &mod = MOD) {
-#if ModOperSafeModOption
-    a %= mod;
-    b %= mod;
-
-    if (a < 0)
-        a += mod;
-
-    if (b < 0)
-        b += mod;
-#endif // ModOperSafeModOption
-
     a = a - b;
 
     if (a < 0)
@@ -49,92 +25,17 @@ void Dec(T1 &a, T2 b, const T3 &mod = MOD) {
 
 template<typename T1, typename T2, typename T3 = valueType>
 T1 sum(T1 a, T2 b, const T3 &mod = MOD) {
-#if ModOperSafeModOption
-    a %= mod;
-    b %= mod;
-
-    if (a < 0)
-        a += mod;
-
-    if (b < 0)
-        b += mod;
-#endif // ModOperSafeModOption
-
     return a + b >= mod ? a + b - mod : a + b;
 }
 
 template<typename T1, typename T2, typename T3 = valueType>
 T1 sub(T1 a, T2 b, const T3 &mod = MOD) {
-#if ModOperSafeModOption
-    a %= mod;
-    b %= mod;
-
-    if (a < 0)
-        a += mod;
-
-    if (b < 0)
-        b += mod;
-#endif // ModOperSafeModOption
-
     return a - b < 0 ? a - b + mod : a - b;
 }
 
 template<typename T1, typename T2, typename T3 = valueType>
 T1 mul(T1 a, T2 b, const T3 &mod = MOD) {
-#if ModOperSafeModOption
-    a %= mod;
-    b %= mod;
-
-    if (a < 0)
-        a += mod;
-
-    if (b < 0)
-        b += mod;
-#endif // ModOperSafeModOption
-
     return (long long) a * b % mod;
-}
-
-template<typename T1, typename T2, typename T3 = valueType>
-void Mul(T1 &a, T2 b, const T3 &mod = MOD) {
-#if ModOperSafeModOption
-    a %= mod;
-    b %= mod;
-
-    if (a < 0)
-        a += mod;
-
-    if (b < 0)
-        b += mod;
-#endif // ModOperSafeModOption
-
-    a = (long long) a * b % mod;
-}
-
-template<typename T1, typename T2, typename T3 = valueType>
-T1 pow(T1 a, T2 b, const T3 &mod = MOD) {
-#if ModOperSafeModOption
-    a %= mod;
-    b %= mod;
-
-    if (a < 0)
-        a += mod;
-
-    if (b < 0)
-        b += mod;
-#endif // ModOperSafeModOption
-
-    T1 result = 1;
-
-    while (b > 0) {
-        if (b & 1)
-            Mul(result, a, mod);
-
-        Mul(a, a, mod);
-        b = b >> 1;
-    }
-
-    return result;
 }
 
 class LineSieve {
@@ -252,7 +153,13 @@ int main() {
         S[i] = sum(C(N - 1, i), S[i - 1]);
 
     for (valueType i = 1; i <= N; ++i) {
-        F[i] = sub(S[N - 1], sum(S[i - 1], S[i - 2]));
+        F[i] = S[N - 1];
+
+        if (i > 0)
+            Dec(F[i], S[i - 1]);
+
+        if (i > 1)
+            Dec(F[i], S[i - 2]);
 
         Inc(F[i], F[i - 1]);
     }
