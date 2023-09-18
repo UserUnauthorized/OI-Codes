568f8989fd13506f712dff526ff3078f0e261f66
Self-Joking <69748465+Self-Joking@users.noreply.github.com>
2023-09-18 15:39:55 +0800

    ARC118C

--- AtCoder/arc118_c.cpp
+++ AtCoder/arc118_c.cpp
@@ -18,22 +18,8 @@ int main() {
     ans.push_back(2 * 5);
     ans.push_back(3 * 5);
 
-    for (valueType i = 12; i <= V; i += 6)
-        if (i % 5 == 0)
-            continue;
-        else
-            ans.push_back(i);
-
-    for (valueType i = 20; i <= V; i += 10)
-        if (i % 3 == 0)
-            continue;
-        else
-            ans.push_back(i);
-
-    for (valueType i = 30; i <= V; i += 15)
-        if (i % 2 == 0)
-            continue;
-        else
+    for(valueType i = 16; i <= V && ans.size() < N; ++i)
+        if(i % 6 == 0 || i % 10 == 0 || i % 15 == 0)
             ans.push_back(i);
 
     for (auto const &iter: ans)
